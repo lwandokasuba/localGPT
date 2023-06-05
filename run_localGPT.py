7328460add98a7eb27a3acd56be2ce9d14b3c080
lwandokasuba <lwando@thinkzambia.org>
2023-06-05 20:35:08 +0200

    direct question

--- run_localGPT.py
+++ run_localGPT.py
@@ -77,28 +77,45 @@ def main(device_type, ):
     llm = load_model()
     qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents=True)
     # Interactive questions and answers
-    while True:
-        query = input("\nEnter a query: ")
-        if query == "exit":
-            break
+#     while True:
+#         query = input("\nEnter a query: ")
+#         if query == "exit":
+#             break
 
-        # Get the answer from the chain
-        res = qa(query)
-        answer, docs = res['result'], res['source_documents']
+#         # Get the answer from the chain
+#         res = qa(query)
+#         answer, docs = res['result'], res['source_documents']
 
-        # Print the result
-        print("\n\n> Question:")
-        print(query)
-        print("\n> Answer:")
-        print(answer)
+#         # Print the result
+#         print("\n\n> Question:")
+#         print(query)
+#         print("\n> Answer:")
+#         print(answer)
 
-        # # Print the relevant sources used for the answer
-        print("----------------------------------SOURCE DOCUMENTS---------------------------")
-        for document in docs:
-            print("\n> " + document.metadata["source"] + ":")
-            print(document.page_content)
-        print("----------------------------------SOURCE DOCUMENTS---------------------------")
+#         # # Print the relevant sources used for the answer
+#         print("----------------------------------SOURCE DOCUMENTS---------------------------")
+#         for document in docs:
+#             print("\n> " + document.metadata["source"] + ":")
+#             print(document.page_content)
+#         print("----------------------------------SOURCE DOCUMENTS---------------------------")
 
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
+    query = "What is a subnet mask"
+    res = qa(query)
+    answer, docs = res['result'], res['source_documents']
+
+    # Print the result
+    print("\n\n> Question:")
+    print(query)
+    print("\n> Answer:")
+    print(answer)
+
+    # # Print the relevant sources used for the answer
+    print("----------------------------------SOURCE DOCUMENTS---------------------------")
+    for document in docs:
+        print("\n> " + document.metadata["source"] + ":")
+        print(document.page_content)
+    print("----------------------------------SOURCE DOCUMENTS---------------------------")
+
